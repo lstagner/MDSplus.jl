35e176f0f6325bd2527300c22d98a2c5472d7d5f
Luke Stagner <lstagner@uci.edu>
2016-09-29 01:11:42 -0700

    Its alive!!!

---
+++ examples/example.jl
@@ -0,0 +1,36 @@
+using MDSplus
+
+function status_ok(stat)
+    (stat & 1) == 1
+end
+
+function get_signal_length(sig)
+    s = [0]
+    idesc = descr(Int32, size(s), s)
+    stat = MdsValue("SIZE($sig)", idesc)
+    return s[1]
+end
+
+function run_example(shot)
+    socket = MdsConnect("atlas.gat.com")
+
+    stat = MdsOpen("EFIT01",shot)
+
+    s = get_signal_length("\\AMINOR")
+
+    data = zeros(Float64, s)
+    sigdesc = descr(data)
+    stat = MdsValue("\\AMINOR", sigdesc)
+
+    time = zeros(Float64, s)
+    timedesc = descr(time)
+    stat = MdsValue("DIM_OF(\\AMINOR)", timedesc)
+
+    for i=1:10
+        println(@sprintf("%2i X: %6.2f Y: %6.2f",i,time[i],data[i]))
+    end
+
+    MdsDisconnect()
+end
+
+run_example(107000)
--- src/MDSplus.jl
+++ src/MDSplus.jl
@@ -1,5 +1,67 @@
 module MDSplus
 
-# package code goes here
+push!(Libdl.DL_LOAD_PATH,ENV["MDS_SHLIB_PATH"])
 
-end # module
+MdsTypes = Dict{DataType,Int32}(UInt8 => 2, UInt16 => 3, UInt32 => 4, UInt64 => 5,
+                                Int8 => 6, Int16 => 7, Int32 => 8, Int64 => 9,
+                                Float32 => 10, Float64 => 11,
+                                Complex{Float32} => 12, Complex{Float64} => 13,
+                                Cstring => 14)
+
+function descr{T,N}(::Type{T}, s::NTuple{N,Int}, data)
+    mds_type = MdsTypes[T]
+    if N == 0
+        ss = map(Int32, (s...,0))
+    else
+        ss = map(Int32,s)
+    end
+    ccall( (:descr, "libMdsLib"), Cint, (Ref{Cint}, Ptr{Void}, Ref{Cint}, Ref{Cint}...), mds_type, Ref(data), ss...,Int32(0))
+end
+
+function descr(data)
+    s = size(data)
+    mds_type = MdsTypes[eltype(data)]
+    descr(eltype(data), s, data)
+end
+
+function MdsConnect(server::AbstractString)
+    ccall( (:MdsConnect, "libMdsLib"), Cint, (Cstring,), server)
+end
+
+function MdsDisconnect()
+    ccall( (:MdsDisconnect, "libMdsLib"), Void, ())
+end
+
+function MdsOpen{T<:Integer}(tree::AbstractString, shot::T)
+    ccall( (:MdsOpen, "libMdsLib"), Cint, (Cstring,Ref{Cint}), tree, Int32(shot))
+end
+
+function MdsClose{T<:Integer}(tree::AbstractString, shot::T)
+    ccall( (:MdsClose, "libMdsLib"), Cint, (Cstring,Ref{Cint}), tree, Int32(shot))
+end
+
+function MdsSetDefault(node::AbstractString)
+    ccall( (:MdsSetDefault, "libMdsLib"), Cint, (Cstring,), node)
+end
+
+function MdsValue{N,T<:Integer}(expression::AbstractString, d::Vararg{T,N})
+    dd = map(Int32, d)
+    len = [Int32(0)]
+    NULL = Int32(0)
+    stat = ccall( (:MdsValue, "libMdsLib"), Cint, (Cstring, Ref{Cint}...), expression, dd..., NULL, len)
+    return stat
+end
+
+function MdsPut{N,T<:Integer}(node::AbstractString, expression::AbstractString, d::Vararg{T,N})
+    dd = map(Int32, d)
+    ccall( (:Mdsput, "libMdsLib"), Cint, (Cstring, Cstring, Ref{Cint}...), expression, dd...,Int32(0))
+end
+
+function MdsSetSocket{T<:Integer}(socket::T)
+    ccall( (:MdsSetSocket, "libMdsLib"), Cint, (Ref{Cint},), Int32(socket))
+end
+
+export descr, MdsConnect, MdsDisconnect, MdsOpen, MdsClose
+export MdsSetDefault, MdsValue, MdsPut, MdsSetSocket
+
+end
